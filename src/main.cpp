e43d8243cc923dc6953ea1aa951379b76a0dd520
Ivan Reshetnyak <ivan.reshetnyak@gmail.com>
2020-12-15 15:29:11 +0300

    Create project files

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/***************************************************************
+ * Copyright (C) 2020
+ *    Reshetnyak Ivan
+ ***************************************************************/
+
+#include "pch.h"
+
+#include <windows.h>
+
+INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    CHAR *CmdLine, INT CmdShow ) {
+  return 0;
+}
