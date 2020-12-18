16acf16cddd0248b6fd7354afd7ade86150b833f
Ivan Reshetnyak <ivan.reshetnyak@gmail.com>
2020-12-18 12:19:13 +0300

    Add 'triangle' unit .cpp file

--- assets/shaders/pass/.frag
+++ assets/shaders/pass/.frag
@@ -7,13 +7,7 @@ in vec2 Tex;
 
 out vec4 OutCol;
 
-uniform vec4 Ka;
-uniform vec4 Kd;
-uniform vec4 Ks;
-
-uniform float Time;
-
 void main( void ) {
-  gl_FragColor = vec4(1, 1, 1, 1);
-  // gl_FragColor = Ka * Col;
+  OutCol = Col;
+  // gl_FragColor = Col;
 }
--- src/anim/units/triangle.h
+++ src/anim/units/triangle.h
@@ -15,25 +15,12 @@ namespace units {
 
 
 class triangle : public unit {
+public:
+  triangle( animation *Anim, const vec &V1, const vec &V2, const vec &V3 );
+  void Render() override;
+
 private:
   prim::trimesh Primitive;
-public:
-  triangle( animation *Anim, const vec &V1, const vec &V2, const vec &V3 ) : unit(Anim), Primitive(Anim) {
-    vertex Vertices[] = {
-      vertex(V1, {0, 0, -1}, {V1.X, V1.Y}, color(1, 1, 1, 1)),
-      vertex(V2, {0, 0, -1}, {V2.X, V2.Y}, color(1, 1, 1, 1)),
-      vertex(V3, {0, 0, -1}, {V3.X, V3.Y}, color(1, 1, 1, 1))
-    };
-    int Indices[] = {0, 1, 2};
-    geometry Geom(3, Vertices, 3, Indices);
-    auto Shader = shader::Manager.Get("pass");
-    auto Material = material::Manager.Add("default", Shader);
-    Primitive.Set(Geom, Material);
-  }
-
-  void Render() override {
-    Primitive.Draw();
-  }
 };
 
 
